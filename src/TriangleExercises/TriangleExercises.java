a8f67f5c74b26bf4575d0ff0c66498f75cb5cebc
Nicolás Oñate <nonate@thoughtworks.com>
2018-02-26 17:04:39 -0300

    Easiest exercise ever

--- src/TriangleExercises/TriangleExercises.java
+++ src/TriangleExercises/TriangleExercises.java
@@ -5,5 +5,12 @@ public class TriangleExercises {
     public static void main(String[] args) {
 
     }
-    
+
+    // Easiest exercise ever
+    public static void easiestExerciseEver() {
+        System.out.println("Easiest Exercise Ever");
+        System.out.println("*");
+    }
+
+
 }
