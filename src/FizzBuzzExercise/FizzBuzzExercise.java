8812c699cb807242af510dbd436fb9f1d374f9ff
Nicolás Oñate <nonate@thoughtworks.com>
2018-02-27 10:53:02 -0300

    FizzBuzz

--- src/FizzBuzzExercise/FizzBuzzExercise.java
+++ src/FizzBuzzExercise/FizzBuzzExercise.java
@@ -3,7 +3,27 @@ package FizzBuzzExercise;
 public class FizzBuzzExercise {
 
     public static void main(String[] args) {
-
+        fizzBuzz();
     }
 
+    // FizzBuzz
+    public static void fizzBuzz() {
+
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }else{
+                if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                }else{
+                    if (i % 5 == 0){
+                        System.out.println("Buzz");
+                    }else{
+                        System.out.println(i);
+                    }
+                }
+            }
+
+        }
+    }
 }
