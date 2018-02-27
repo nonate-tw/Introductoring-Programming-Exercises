51b1f3ef3bf4e20e198a3b95da8c3580311f1b98
Nicolás Oñate <nonate@thoughtworks.com>
2018-02-27 10:11:55 -0300

    Diamond

--- src/DiamondExercises/DiamondExercises.java
+++ src/DiamondExercises/DiamondExercises.java
@@ -3,12 +3,11 @@ package DiamondExercises;
 public class DiamondExercises {
 
     public static void main(String[] args) {
-        isoscelesTriangle(3);
+        diamond(3);
     }
 
     // Isosceles Triangle
     public static void isoscelesTriangle(int n) {
-        System.out.println("Isosceles Triangle");
         int b = n;
         int extra = 0;
         for(int count = 1; count <= n; ++count){
@@ -21,6 +20,27 @@ public class DiamondExercises {
             System.out.println("");
             b--;
             extra++;
+        }
+    }
+
+    // Diamond
+    public static void diamond(int n) {
+        System.out.println("Diamond");
+
+        isoscelesTriangle(n);
+
+        int b = n-1;
+        int extra = n-1;
+        for(int count = n-1; count >= 1; --count){
+            for(int xcount = 1; xcount < b; ++xcount){
+                System.out.print(" ");
+            }
+            for(int tcount = 1; tcount < count+extra; ++tcount){
+                System.out.print("*");
+            }
+            System.out.println("");
+            b++;
+            extra--;
         }
     }
 }
