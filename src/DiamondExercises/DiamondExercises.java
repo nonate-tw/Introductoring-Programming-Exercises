a4432db130f15eaeacea08dd7c5b7aa0dc72cfb5
Nicolás Oñate <nonate@thoughtworks.com>
2018-02-27 09:54:22 -0300

    Isosceles Triangle

--- src/DiamondExercises/DiamondExercises.java
+++ src/DiamondExercises/DiamondExercises.java
@@ -3,8 +3,24 @@ package DiamondExercises;
 public class DiamondExercises {
 
     public static void main(String[] args) {
-
+        isoscelesTriangle(3);
     }
 
-    
+    // Isosceles Triangle
+    public static void isoscelesTriangle(int n) {
+        System.out.println("Isosceles Triangle");
+        int b = n;
+        int extra = 0;
+        for(int count = 1; count <= n; ++count){
+            for(int xcount = 1; xcount < b; ++xcount){
+                System.out.print(" ");
+            }
+            for(int tcount =1; tcount <= count + extra; ++tcount){
+                System.out.print("*");
+            }
+            System.out.println("");
+            b--;
+            extra++;
+        }
+    }
 }
