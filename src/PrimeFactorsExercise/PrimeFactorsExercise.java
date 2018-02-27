826f3604f4595fc0382793184adb8e8aa21007e3
Nicolás Oñate <nonate@thoughtworks.com>
2018-02-27 11:56:46 -0300

    Prime Factors update

--- src/PrimeFactorsExercise/PrimeFactorsExercise.java
+++ src/PrimeFactorsExercise/PrimeFactorsExercise.java
@@ -3,21 +3,21 @@ package PrimeFactorsExercise;
 public class PrimeFactorsExercise {
 
     public static void main(String[] args) {
-        generate(42);
+        generate(30);
     }
 
     // Prime Factors
     public static void generate(int n) {
         String out = "";
-        if (n > 1) {
-            for (int i = 2; i <= n; i++) {
 
-                while (n % i == 0){
-                    out += i + ",";
-                    n /= i;
-                }
+        for (int i = 2; i <= n; i++) {
+
+            while (n % i == 0){
+                out += i + ",";
+                n /= i;
             }
         }
+
         System.out.println(out.substring(0, out.length()-1));
     }
 }
